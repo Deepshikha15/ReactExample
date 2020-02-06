edd87142c8f9e321e29bb855cdba4905256d684f
Deepshikha <deepshikha.mishra@moneyview.in>
2020-02-06 13:35:06 +0530

    list

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
   state = { 
     person:[
       {
+        id:"1",
         name:"Harry"
       },
       {
+        id:"2",
         name:"Ron"
       }],
 
@@ -23,7 +25,7 @@ class App extends Component {
            name:newName
          },
          {
-           name:"Voldemote"
+           name:"Voldemort"
          }
        ]
      })
@@ -33,7 +35,22 @@ class App extends Component {
     //  this.state.person[1].name="Voldemort";  
    }
 
-   namechangeHandler= (event) =>{
+   namechangeHandler= (event ) =>{
+
+    // const personIndex = this.state.person.findIndex(p=>{
+    //   return p.id = id;
+    // })
+
+    // const person = {
+    //   ...this.state.person[personIndex]
+    // }
+
+    // person = event.target.value;
+
+    // const person = [...this.state.person]
+
+    // person[personIndex] = person;
+  
     this.setState({
       person:[
         {
@@ -52,19 +69,48 @@ class App extends Component {
      
    }
 
+   deleteHandler = (personIndex) =>{
+     const person = this.state.person
+     person.splice(personIndex,1)
+     this.setState({person:person})
+
+   }
+
 
 
   render() { 
+    let person = null
+
+    if(this.state.showPerson){
+      person = (
+        <div>
+          {this.state.person.map((person,index) =>{
+            return <Page name={person.name}
+            click={()=> this.deleteHandler(index)}
+            key={person.id}
+          />
+            
+          }
+          )}
+          {/* <Page name={this.state.person[0].name}
+          change={this.namechangeHandler}/>
+          <Page name={this.state.person[1].name}
+          click={this.textChangeOnClick.bind(this, 'nagini')}> am wizard at Hogwarts</Page> */}
+      </div>
+      );
+    }
+
     return ( 
       <div className="App">
       <button onClick={this.toggleHandler}>Change</button>
-      { this.state.showPerson === true ? 
+      {/* { this.state.showPerson === true ? 
         <div>
-        <Page name={this.state.person[0].name}
+          <Page name={this.state.person[0].name}
           change={this.namechangeHandler}/>
           <Page name={this.state.person[1].name}
           click={this.textChangeOnClick.bind(this, 'nagini')}> am wizard at Hogwarts</Page>
-      </div> : null}
+      </div> : null} */}
+      {person}
       
     </div>
      );
